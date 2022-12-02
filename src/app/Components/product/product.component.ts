22c75f889a37c296f87afcdaaaac5adacb4891ce
Jatinamin <jatinamin23@gmail.com>
2022-12-02 14:32:07 -0500

    2 files modified, 4 added

---
+++ src/app/Components/product/product.component.ts
@@ -0,0 +1,52 @@
+import { Component, OnInit } from '@angular/core';
+import { Product } from 'src/app/model/product.model';
+@Component({
+  selector: 'app-details',
+  templateUrl: './details.component.html',
+  styleUrls: ['./details.component.scss']
+})
+export class DetailsComponent implements OnInit {
+  //Array Binding
+  products: Array<Product> = [
+    {
+      productID: 1,
+      productName: "Laptop",
+      productDetails: "Details"
+    },
+    {
+      productID: 2,
+      productName: "CellPhone",
+      productDetails: "Details"
+    },
+    {
+      productID: 3,
+      productName: "Ipad",
+      productDetails: "Details"
+    }
+  ]
+//variable binding
+  // product: Product =
+  //   {
+  //     productID: 1,
+  //     productName: "Laptop",
+  //     productDetails: "Details"
+  //   }
+  // product1: Product = {
+  //   productID: 2,
+  //   productName: "CellPhone",
+  //   productDetails: "Details"
+  // }
+
+  // product2: Product = {
+  //   productID: 3,
+  //   productName: "IPad",
+  //   productDetails: "Details"
+  // }
+
+  constructor() { /* TODO document why this constructor is empty */ }
+
+  ngOnInit(): void {
+    // TODO document why this method 'ngOnInit' is empty
+  }
+
+}
---
+++ src/app/model/product.model.ts
@@ -0,0 +1,7 @@
+
+
+export class Product{
+  productID: number =0;
+  productName:  string ="";
+  productDetails: string ="";
+}
